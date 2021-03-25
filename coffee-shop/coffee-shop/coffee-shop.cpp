51f058dc401c4697dc25337408704013cfcdfcf3
kddinev18 <kddinev18@codingburgas.bg>
2021-03-25 15:03:57 +0200

    add COFFEE structure

---
+++ coffee-shop/coffee-shop/coffee-shop.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+struct COFFEE {
+	string articul;
+	int count = 0;
+	float price = 0;
+	COFFEE* next = NULL;
+};
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
